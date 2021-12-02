9cbb4de3a107088e0a6ed72f17a4714b3666aa00
Joseeph5 <youssef.beenamor@gmail.com>
2021-12-02 17:28:04 +0100

    change site title

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <nav>
-      <div className="nav-center">
-        <Link to="/">
-          <h2>MealsToCook</h2>
+      <div className='nav-center'>
+        <Link to='/'>
+          <h2>MealToCook</h2>
         </Link>
-        <div className="nav-header"></div>
-        <ul className="nav-links">
+        <div className='nav-header'></div>
+        <ul className='nav-links'>
           <li>
-            <Link to="/">Home</Link>
+            <Link to='/'>Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to='/about'>About</Link>
           </li>
         </ul>
       </div>
